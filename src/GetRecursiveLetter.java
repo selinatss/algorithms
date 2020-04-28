75b6cd5d7e021d15da1773fb2e3c4277dcd4dd84
selin.atis <selinatiss@gmail.com>
2020-04-28 09:15:15 +0300

    GetRecursiveLetter algorithms has been added.

---
+++ src/GetRecursiveLetter.java
@@ -0,0 +1,40 @@
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class GetRecursiveLetter {
+    public static void main(String[] args){
+        String sentence = "hello";
+        getRecurringCharacter(sentence);
+    }
+
+    //Sonuçlar büyük harf
+    //Büyük O ve küçük O aynı, input almalı
+    public static void getRecurringCharacter(String sentence) {
+        if(sentence == null){
+            System.out.println("There aren't any recurring object");
+        }
+        sentence = sentence.toUpperCase();
+        char[] charSentence = sentence.toCharArray();
+
+        HashMap<Character, Integer> count = new HashMap<>();
+        for(int i=0; i< charSentence.length; i++){
+            if(!count.containsKey(charSentence[i])) {
+                count.put(charSentence[i], 1);
+            }else
+            {
+                count.put(charSentence[i], 2);
+            }
+        }
+
+        Iterator<Map.Entry<Character, Integer>> iterator = count.entrySet().iterator();
+        while (iterator.hasNext()){
+            Map.Entry<Character, Integer> entry = iterator.next();
+            if(entry.getValue() > 1){
+                System.out.println(entry.getKey().toString());
+            }
+        }
+
+    }
+
+}
